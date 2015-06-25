2684785168c06ac542fbcbcff4896b7e75c01a82
Pablo Margreff <pablo_margreff@hotmail.com>
2015-06-25 20:57:19 -0300

    Initial commit

---
+++ Node.java
@@ -0,0 +1,14 @@
+/*************************************************************************
+ *  YOU DO NOT NEED TO MODIFY THIS FILE
+ *
+ *  Dependencies: Node.java
+ *  Compilation:  javac Node.java
+ *
+ *  Data type for linked lists of Points
+ *
+ *************************************************************************/
+
+public class Node {
+  public Point p;
+  public Node next;
+}
