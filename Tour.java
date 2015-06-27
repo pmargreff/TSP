118f3cd5c17d18060a724dbf402854882c93f170
Pablo Margreff <pablo_margreff@hotmail.com>
2015-06-27 14:18:42 -0300

    corrected insertNearest

--- Tour.java
+++ Tour.java
@@ -82,43 +82,51 @@ public class Tour {
         double dist = 0.0;
         if (this.head_.p != null) {
             Node tmp;
+
             for (tmp = this.head_; tmp.next != this.head_; tmp = tmp.next) {
                 dist += tmp.p.distanceTo(tmp.next.p);
             }
-            dist += tmp.p.distanceTo(tmp.next.p);
+
+            dist += tmp.p.distanceTo(this.head_.p);
         }
         return dist;
     }
-    
+
     /*
-    *   ver por que está dando um resultado melhor 
-    *   que o esperado (mas não o ótimo)
-    */
+     *   ver por que está dando um resultado melhor 
+     *   que o esperado (mas não o ótimo)
+     */
     void insertNearest(Point newPoint) {                   // insert p using nearest neighbor heuristic
+        //caso a lista estiver vazia insere no inicio
         if (this.head_.p == null) {
-            
+
             this.head_.p = newPoint;
             this.head_.next = head_;
 
-        } else{
-            double dist = this.head_.p.distanceTo(newPoint);
+        } else {
+            double dist = this.head_.p.distanceTo(newPoint); //salva a distância do primeiro
+            Node near = this.head_; //e inicia usando para o mais perto
+            
             Node tmp;
-            Node near = new Node();
-            near = this.head_;
-            //go to ond of list
+            //percorre a lista
             for (tmp = this.head_; tmp.next != this.head_; tmp = tmp.next) {
 //                System.out.print(tmp.p.distanceTo(newPoint)+" ");
-                
-                if (dist > tmp.p.distanceTo(newPoint)){
+
+                //caso seja o primeiro salve em near e atualiza a distância
+                if (dist > tmp.p.distanceTo(newPoint)) {
                     dist = tmp.p.distanceTo(newPoint);
                     near = tmp;
                 }
-                
+
             }
-//
+            
+            if (dist > tmp.p.distanceTo(newPoint)) {
+                    near = tmp;
+                }
 //            System.out.println(dist);
 //            System.out.println();
-            
+
+            //insere o novo nodo após e mais próximo
             Node newNode = new Node();
             newNode.p = newPoint;
             newNode.next = near.next;   //point the new to next
